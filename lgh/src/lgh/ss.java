8c0a2b4f41b3cfbe646969cb6b162e3eaabe5e5d
lgh1914 <lgh1914@naver.com>
2019-03-27 11:29:23 +0900

    master branch change a code

--- lgh/src/lgh/ss.java
+++ lgh/src/lgh/ss.java
@@ -4,5 +4,7 @@ public class ss {
 public static void main(String[]args) {
 	System.out.println("Hello World");
 	System.out.println("world's End Dancehall");
+	System.out.println("Earth'S End Dancehall");
+	
 }
 }
